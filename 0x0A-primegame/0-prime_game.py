7232183e5c72141c8040206e56305fe1a0ed9f83
Tolera Degefa <100357038+todaycu@users.noreply.github.com>
2023-03-08 21:39:01 +0300

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,44 +1,37 @@
 #!/usr/bin/python3
 """
-0. Island Perimeter
+    Game of choosing Prime numbers
 """
 
 
-def island_perimeter(grid):
+def primeNumbers(n):
+    """Return list of prime numbers between 1 and n
     """
-    Function to find the perimeter of an island
-    Parameters
-    ----------
-    grid : list
-      list of list of integers 0 represents water and 1 represents
-    Returns
-    -------
-      perimeter of the island described in grid
+    primeNos = []
+    filtered = [True] * (n + 1)
+    for prime in range(2, n + 1):
+        if (filtered[prime]):
+            primeNos.append(prime)
+            for i in range(prime, n + 1, prime):
+                filtered[i] = False
+    return primeNos
+
+
+def isWinner(x, nums):
     """
-    if not grid or any(type(arr) != list for arr in grid):
-        return 0
-    width = len(grid[0]) - 1
-    if any(len(arr) != width + 1 for arr in grid):
-        return 0
-    islands = connections = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j]:
-                islands += 1
-                if j < len(grid[0]) - 1 and grid[i][j + 1] == 1:
-                    connections += 1
-                if i < len(grid) - 1 and grid[i + 1][j] == 1:
-                    connections += 1
-    return 4 * islands - 2 * connections
-
-
-if __name__ == "__main__":
-    grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
-    assert(island_perimeter(grid) == 12)
-
+    Determines winner
+    """
+    if x is None or nums is None or x == 0 or nums == []:
+        return None
+    Maria = Ben = 0
+    for i in range(x):
+        primeNos = primeNumbers(nums[i])
+        if len(primeNos) % 2 == 0:
+            Ben += 1
+        else:
+            Maria += 1
+    if Maria > Ben:
+        return 'Maria'
+    elif Ben > Maria:
+        return 'Ben'
+    return None
